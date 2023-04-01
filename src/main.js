0d80d1140577ebf7ece4b35940ed24cc3a86c2b4
Timo Bühlmann <timo.buehlmann@gmail.com>
2023-04-01 20:19:32 +0200

    cleanup

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import backup from "./backup.js";
 
 // these intervals should be written from the ascending otherwise it is possible, that the script won't work properly
 // `0D' is needed so the new backup isn't cleaned away instantly
-const backupIntervals = ['0D', '1D', '2D', '3D', '1W', '2W', '1Y'];
+const backupIntervals = ['0D', '1D', '2D', '3D', '1W', '2W', '1M', '2M', '3M', '6M', '1Y', '2Y', '3Y', '10Y', '20Y', '30Y', '100Y'];
 
 // use slash at the end so only the content of the folder will be copied
 const sourceDir = 'source/';
@@ -11,9 +11,5 @@ const destinationDir = 'backups';
 // IMPORTANT the leading "/" represents the source folder
 const excludedDirectories = [];
 
-//for testing
-const currentDate = new Date();
-currentDate.setDate((new Date()).getDate() + 108);
-
-backup(sourceDir, destinationDir, excludedDirectories, currentDate);
-cleanup(destinationDir, backupIntervals, currentDate);
+backup(sourceDir, destinationDir, excludedDirectories, new Date());
+cleanup(destinationDir, backupIntervals, new Date());
