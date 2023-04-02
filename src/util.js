7e4ac232898a5fca027ab565461bdb42f6a4f1cd
Timo Bühlmann <timo.buehlmann@gmail.com>
2023-04-02 15:53:27 +0200

    util(exec): remove resolve for command

--- src/util.js
+++ src/util.js
@@ -3,7 +3,7 @@ import path from "path";
 
 export function exec(command, name = 'COMMAND') {
   console.log(`${name}: ${command}`);
-  execSync(path.resolve(command), (error, stdout, stderr) => {
+  execSync(command, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error occurred: ${error}`);
       process.exit(1);
